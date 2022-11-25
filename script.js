ac3c4100849455639d298ad1760859989e0f30fc
Pedro Elias <106087744+Elias-Pdr@users.noreply.github.com>
2022-11-25 17:39:52 -0300

    up

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function clickItem(src) {
-    localStorage.setItem("srcItem", src);
+function clickItem(idImg) {
+
+    localStorage.setItem("srcItem", document.getElementById(idImg).src);
     console.log(localStorage.getItem("srcItem"));
     window.location.href = "telaItem.html"
 }
+
+document.getElementById('img1').src = 'img/1.png'
+document.getElementById('img2').src = 'img/2.png'
+document.getElementById('img3').src = 'img/3.png'
+document.getElementById('img4').src = 'img/4.png'
+document.getElementById('img5').src = 'img/5.png'
+document.getElementById('img6').src = 'img/6.png'
+document.getElementById('img7').src = 'img/7.png'
+document.getElementById('img8').src = 'img/8.png'
+document.getElementById('img9').src = 'img/9.png'
+document.getElementById('img10').src = 'img/10.png'
+document.getElementById('img11').src = 'img/11.png'
+document.getElementById('img12').src = 'img/12.png'
+document.getElementById('img13').src = 'img/13.png'
+document.getElementById('img14').src = 'img/14.png'
+document.getElementById('img15').src = 'img/15.png'
+document.getElementById('img16').src = 'img/16.png'
+document.getElementById('img17').src = 'img/17.png'
+document.getElementById('img18').src = 'img/18.png'
+document.getElementById('img19').src = 'img/19.png'
+document.getElementById('img20').src = 'img/20.png'
